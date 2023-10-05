6cc19097e6f03730ef3b3607ee9fa91b4fe5eeb6
ccorrea <ccorrea@einsted.bio>
2023-10-05 16:41:01 -0300

    Haciendo responsive la página de inicio.

--- version 1.1/main.js
+++ version 1.1/main.js
@@ -6,14 +6,15 @@ const url  = require('url')
 let win
 
 function createWindow(){
-	win = new BrowserWindow ({
-		width: 1480,
-    	height: 925,
-    	minWidth: 1480,
-    	minHeight: 950,
-		maxWidth: 1480,
-    	maxHeight: 950,
-		frame:true,      // quita los bordes en la ventana
+	win = new BrowserWindow ({ //el problema no es en sí el tamaño de la pantalla, sino que el contenido no es responsive
+		
+		width: 1300,
+    	height: 890, 
+    	minWidth: 1300,
+    	minHeight: 890,
+		maxWidth: 1920,
+    	maxHeight: 1080,
+		frame: true,      // quita los bordes en la ventana
 		transparent: false, // hace la ventana trasparente
 
     	webPreferences: {
@@ -29,7 +30,7 @@ function createWindow(){
 	win.removeMenu();
 	win.setMenu(null);           // Elimina el menu de la aplicación
 
-	//win.webContents.openDevTools()
+	// win.webContents.openDevTools()
 
 
 
